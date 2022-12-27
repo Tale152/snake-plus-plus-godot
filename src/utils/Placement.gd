5d36dfe6d16e13879b5dab0c783727676532ef28
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-12-27 12:44:05 +0100

    created Placement

---
+++ src/utils/Placement.gd
@@ -0,0 +1,9 @@
+class Placement:
+	var position
+	var next_direction
+	var previous_direction
+	
+	func _init(position, next_direction, previous_direction):
+		position = position
+		next_direction = next_direction
+		previous_direction = previous_direction
