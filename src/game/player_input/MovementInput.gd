890e01e0e530f058c1073128cbd6b3cae07850f4
Tale152 <alessandro.talmi@gmail.com>
2023-01-15 23:51:06 +0100

    created MovementInputs

---
+++ src/game/player_input/MovementInput.gd
@@ -0,0 +1,18 @@
+class_name MovementInput extends Reference
+
+const _ACTION_MOVE_RIGHT: String = "move_right"
+const _ACTION_MOVE_LEFT: String = "move_left"
+const _ACTION_MOVE_UP: String = "move_up"
+const _ACTION_MOVE_DOWN: String = "move_down"
+
+static func get_action_move_right() -> String:
+	return _ACTION_MOVE_RIGHT
+
+static func get_action_move_left() -> String:
+	return _ACTION_MOVE_LEFT
+
+static func get_action_move_up() -> String:
+	return _ACTION_MOVE_UP
+
+static func get_action_move_down() -> String:
+	return _ACTION_MOVE_DOWN
