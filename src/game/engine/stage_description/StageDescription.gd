cc9df0838436190ccb43503225298c89c37fdb5b
Tale152 <alessandro.talmi@gmail.com>
2023-01-15 21:13:58 +0100

    added snake initial direction

--- src/game/engine/stage_description/StageDescription.gd
+++ src/game/engine/stage_description/StageDescription.gd
@@ -2,15 +2,24 @@ class_name StageDescription extends Reference
 
 var _size: FieldSize
 var _snake_spawn_point: ImmutablePoint
+var _snake_initial_direction: int
 
-func _init(size: FieldSize, snake_spawn_point: ImmutablePoint):
+func _init(
+	size: FieldSize,
+	snake_spawn_point: ImmutablePoint,
+	snake_initial_direction: int
+	):
 	_size = size
 	_snake_spawn_point = snake_spawn_point
+	_snake_initial_direction = snake_initial_direction
 
 func get_field_size() -> FieldSize:
 	return _size
 
 func get_snake_spawn_point() -> ImmutablePoint:
 	return _snake_spawn_point
+
+func get_snake_initial_direction() -> int:
+	return _snake_initial_direction
 
 # TODO add more fields
--- src/game/engine/stage_description/StageDescriptionBuilder.gd
+++ src/game/engine/stage_description/StageDescriptionBuilder.gd
@@ -1,7 +1,10 @@
 class_name StageDescriptionBuilder extends Reference
 
-var _size: FieldSize
-var _snake_spawn_point: ImmutablePoint
+const DIRECTIONS = preload("res://src/enums/DirectionsEnum.gd").DIRECTIONS
+
+var _size: FieldSize = null
+var _snake_spawn_point: ImmutablePoint = null
+var _snake_initial_direction: int = -1
 
 func set_field_size(size: FieldSize) -> StageDescriptionBuilder:
 	_size = size
@@ -11,18 +14,42 @@ func set_snake_spawn_point(point: ImmutablePoint) -> StageDescriptionBuilder:
 	_snake_spawn_point = point
 	return self
 
+func set_snake_initial_direction(direction: int) -> StageDescriptionBuilder:
+	_snake_initial_direction = direction
+	return self
+
 func build() -> StageDescription:
-	if _is_field_size_valid() \
-		&& _is_snake_spawn_point_valid():
-		return StageDescription.new(_size, _snake_spawn_point)
-	return null
-	
+	if _all_check_pass():
+		return StageDescription.new(
+			_size,
+			_snake_spawn_point,
+			_snake_initial_direction
+		)
+	else:
+		return null
+
+func _all_check_pass() -> bool:
+	return (
+		_is_field_size_valid()
+		&& _is_snake_spawn_point_valid()
+		&& _is_snake_initial_direction_valid()
+	)
 func _is_field_size_valid():
 	return _size != null && _size.get_height() > 0 && _size.get_width() > 0
 
 func _is_snake_spawn_point_valid():
-	return _snake_spawn_point != null \
-		&& _snake_spawn_point.get_x() >= 0 \
-		&& _snake_spawn_point.get_x() < _size.get_width() \
-		&& _snake_spawn_point.get_y() >= 0 \
+	return (
+		_snake_spawn_point != null
+		&& _snake_spawn_point.get_x() >= 0
+		&& _snake_spawn_point.get_x() < _size.get_width()
+		&& _snake_spawn_point.get_y() >= 0
 		&& _snake_spawn_point.get_y() < _size.get_height()
+	)
+
+func _is_snake_initial_direction_valid():
+	return (
+		_snake_initial_direction == DIRECTIONS.UP
+		|| _snake_initial_direction == DIRECTIONS.DOWN
+		|| _snake_initial_direction == DIRECTIONS.LEFT
+		|| _snake_initial_direction == DIRECTIONS.RIGHT
+	)
