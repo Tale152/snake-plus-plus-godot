123befe7ddfcb8a6e3b5e90a157895b5f0e8ceae
Tale152 <alessandro.talmi@gmail.com>
2023-01-15 20:51:17 +0100

    Added snake's spawn point

--- src/game/engine/stage_description/StageDescription.gd
+++ src/game/engine/stage_description/StageDescription.gd
@@ -1,11 +1,16 @@
 class_name StageDescription extends Reference
 
 var _size: FieldSize
+var _snake_spawn_point: ImmutablePoint
 
-func _init(size: FieldSize):
+func _init(size: FieldSize, snake_spawn_point: ImmutablePoint):
 	_size = size
+	_snake_spawn_point = snake_spawn_point
 
 func get_field_size() -> FieldSize:
 	return _size
+
+func get_snake_spawn_point() -> ImmutablePoint:
+	return _snake_spawn_point
 
 # TODO add more fields
--- src/game/engine/stage_description/StageDescriptionBuilder.gd
+++ src/game/engine/stage_description/StageDescriptionBuilder.gd
@@ -1,15 +1,28 @@
 class_name StageDescriptionBuilder extends Reference
 
 var _size: FieldSize
+var _snake_spawn_point: ImmutablePoint
 
 func set_field_size(size: FieldSize) -> StageDescriptionBuilder:
 	_size = size
 	return self
 
+func set_snake_spawn_point(point: ImmutablePoint) -> StageDescriptionBuilder:
+	_snake_spawn_point = point
+	return self
+
 func build() -> StageDescription:
-	if _is_field_size_valid():
-		return StageDescription.new(_size)
+	if _is_field_size_valid() \
+		&& _is_snake_spawn_point_valid():
+		return StageDescription.new(_size, _snake_spawn_point)
 	return null
 	
 func _is_field_size_valid():
 	return _size != null && _size.get_height() > 0 && _size.get_width() > 0
+
+func _is_snake_spawn_point_valid():
+	return _snake_spawn_point != null \
+		&& _snake_spawn_point.get_x() >= 0 \
+		&& _snake_spawn_point.get_x() < _size.get_width() \
+		&& _snake_spawn_point.get_y() >= 0 \
+		&& _snake_spawn_point.get_y() < _size.get_height()
