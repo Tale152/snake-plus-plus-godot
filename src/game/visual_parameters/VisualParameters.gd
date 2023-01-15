13114b825b8daf231cc81519a3befd188c8539c2
Tale152 <alessandro.talmi@gmail.com>
2023-01-15 22:40:39 +0100

    created VisualParameters

---
+++ src/game/visual_parameters/VisualParameters.gd
@@ -0,0 +1,9 @@
+class_name VisualParameters extends Reference
+
+var _cell_pixels_size: int
+
+func _init(cell_pixels_size: int):
+	_cell_pixels_size = cell_pixels_size
+
+func get_cell_pixels_size() -> int:
+	return _cell_pixels_size
