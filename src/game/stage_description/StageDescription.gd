6994881ee4e4d91ba3be05744c2f99fde1048ff3
Tale152 <alessandro.talmi@gmail.com>
2023-01-15 22:52:16 +0100

    added base delta seconds and speedup factor to StageDescription (and builder)

--- src/game/stage_description/StageDescription.gd
+++ src/game/stage_description/StageDescription.gd
@@ -3,15 +3,21 @@ class_name StageDescription extends Reference
 var _size: FieldSize
 var _snake_spawn_point: ImmutablePoint
 var _snake_initial_direction: int
+var _snake_base_delta_seconds: float
+var _snake_speedup_factor: float
 
 func _init(
 	size: FieldSize,
 	snake_spawn_point: ImmutablePoint,
-	snake_initial_direction: int
-	):
+	snake_initial_direction: int,
+	snake_base_delta_seconds: float,
+	snake_speedup_factor: float
+):
 	_size = size
 	_snake_spawn_point = snake_spawn_point
 	_snake_initial_direction = snake_initial_direction
+	_snake_base_delta_seconds = snake_base_delta_seconds
+	_snake_speedup_factor = snake_speedup_factor
 
 func get_field_size() -> FieldSize:
 	return _size
@@ -22,4 +28,8 @@ func get_snake_spawn_point() -> ImmutablePoint:
 func get_snake_initial_direction() -> int:
 	return _snake_initial_direction
 
-# TODO add more fields
+func get_snake_base_delta_seconds() -> float:
+	return _snake_base_delta_seconds
+
+func get_snake_speedup_factor() -> float:
+	return _snake_speedup_factor
--- src/game/stage_description/StageDescriptionBuilder.gd
+++ src/game/stage_description/StageDescriptionBuilder.gd
@@ -5,6 +5,9 @@ const DIRECTIONS = preload("res://src/enums/DirectionsEnum.gd").DIRECTIONS
 var _size: FieldSize = null
 var _snake_spawn_point: ImmutablePoint = null
 var _snake_initial_direction: int = -1
+var _snake_base_delta_seconds: float = -1
+var _snake_speedup_factor: float = -1
+
 
 func set_field_size(size: FieldSize) -> StageDescriptionBuilder:
 	_size = size
@@ -18,12 +21,22 @@ func set_snake_initial_direction(direction: int) -> StageDescriptionBuilder:
 	_snake_initial_direction = direction
 	return self
 
+func set_snake_base_delta_seconds(delta_seconds) -> StageDescriptionBuilder:
+	_snake_base_delta_seconds = delta_seconds
+	return self
+
+func set_snake_speedup_factor(speedup_factor: float) -> StageDescriptionBuilder:
+	_snake_speedup_factor = speedup_factor
+	return self
+
 func build() -> StageDescription:
 	if _all_check_pass():
 		return StageDescription.new(
 			_size,
 			_snake_spawn_point,
-			_snake_initial_direction
+			_snake_initial_direction,
+			_snake_base_delta_seconds,
+			_snake_speedup_factor
 		)
 	else:
 		return null
@@ -33,6 +46,8 @@ func _all_check_pass() -> bool:
 		_is_field_size_valid()
 		&& _is_snake_spawn_point_valid()
 		&& _is_snake_initial_direction_valid()
+		&& _is_snake_base_delta_seconds_valid()
+		&& _is_snake_speedup_factor_valid()
 	)
 func _is_field_size_valid():
 	return _size != null && _size.get_height() > 0 && _size.get_width() > 0
@@ -53,3 +68,9 @@ func _is_snake_initial_direction_valid():
 		|| _snake_initial_direction == DIRECTIONS.LEFT
 		|| _snake_initial_direction == DIRECTIONS.RIGHT
 	)
+
+func _is_snake_base_delta_seconds_valid():
+	return _snake_base_delta_seconds > 0
+
+func _is_snake_speedup_factor_valid():
+	return _snake_speedup_factor > 0
