ee31b6ac2f6a62f1773f3f63efabafab382afcca
Tale152 <alessandro.talmi@gmail.com>
2023-01-17 12:27:23 +0100

    added spawn parameters and get_placement function to BadApple

--- src/game/edibles/instantaneous/BadApple.gd
+++ src/game/edibles/instantaneous/BadApple.gd
@@ -2,24 +2,38 @@ extends Area2D
 
 signal snake_head_collision(collidable)
 
-var e
-var s
+var _placement: ImmutablePoint
+var _rules: InstantaneousEdibleRules
+var _game
+var _snake
 
 func get_type():
 	return InstantaneousEdiblesTypes.BAD_APPLE()
-	
-func spawn(spawn_position, snake, engine):
-	s = snake
-	e = engine
-	position = spawn_position
-	self.connect("snake_head_collision", snake, "on_collision")
+
+func spawn(
+	placement: ImmutablePoint,
+	rules: InstantaneousEdibleRules,
+	snake,
+	game,
+	visual_parameters: VisualParameters
+):
+	_placement = placement
+	_rules
+	_snake = snake
+	_game = game
+	var px = visual_parameters.get_cell_pixels_size()
+	position = Vector2(placement.get_x() * px, placement.get_y() * px)
+	self.connect("snake_head_collision", _snake, "on_collision")
+
+func get_placement() -> ImmutablePoint:
+	return _placement
 
 func _on_BadApple_area_entered(area):
-	if area == s.get_node("Head"):
+	if area == _snake.get_node("Head"):
 		emit_signal("snake_head_collision", self)
 		self.hide()
-		e.remove_edible(self)
+		_game.remove_edible(self)
 
 func on_snake_head_collision():
-	if s.properties.potential_length > 1:
-		s.properties.potential_length -= 1
+	if _snake.properties.potential_length > 1:
+		_snake.properties.potential_length -= 1
