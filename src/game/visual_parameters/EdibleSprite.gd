ed65e71054bbc855f194678b5bd58c64b259290e
Tale152 <alessandro.talmi@gmail.com>
2023-01-19 13:30:36 +0100

    Updated EdibleSprite to use AssetFiles utility functions

--- src/game/visual_parameters/EdibleSprite.gd
+++ src/game/visual_parameters/EdibleSprite.gd
@@ -8,14 +8,12 @@ func _init(path: String, name: String):
 	var sprite: AnimatedSprite = AnimatedSprite.new()
 	sprite.frames = SpriteFrames.new()
 	sprite.frames.add_animation("default")
-	var dir = Directory.new()
 	var i = 0
 	var searching_for_sprites = true
 	while searching_for_sprites:
-		var f = "/" + name + "_" + str(i) + ".png"
-		if dir.file_exists(path + f):
-			var img: StreamTexture = load(path + f)
-			sprite.frames.add_frame("default", img, i)
+		var f = AssetFiles.build_asset_path(path, name, i)
+		if AssetFiles.asset_exists(f):
+			sprite.frames.add_frame("default", AssetFiles.load_asset(f), i)
 			i += 1
 		else:
 			searching_for_sprites = false
