03e7bc3fa182e217c0411922ba0fe485e6cb3a66
Tale152 <alessandro.talmi@gmail.com>
2023-01-26 17:28:12 +0100

    Changed arguments name to avoid unused argument warning

--- src/game/edibles/types/BadApple.gd
+++ src/game/edibles/types/BadApple.gd
@@ -1,8 +1,13 @@
 class_name BadApple extends Reference
 
-func execute(placement, rules, snake, game):
-	if snake.get_properties().get_potential_length() > 1:
-		snake.get_properties().set_potential_length(
-			snake.get_properties().get_potential_length() - 1
+func execute(
+	_placement,
+	_rules,
+	_snake,
+	_game
+) -> bool:
+	if _snake.get_properties().get_potential_length() > 1:
+		_snake.get_properties().set_potential_length(
+			_snake.get_properties().get_potential_length() - 1
 		)
 	return true
