3c7d674934e0efde7fd203e8a4c0712609f03b0e
Tale152 <alessandro.talmi@gmail.com>
2023-01-26 15:24:41 +0100

    Refactored Properties.gd

--- src/game/snake/Properties.gd
+++ src/game/snake/Properties.gd
@@ -1,10 +1,33 @@
-class Properties extends Reference:
-	var current_length
-	var potential_length
-	var current_direction
-	var can_input_direction
-	
-	func _init(initial_direction):
-		current_length = 1
-		potential_length = 1
-		current_direction = initial_direction
+class_name Properties extends Reference
+
+var _current_length: int = 1
+var _potential_length: int = 1
+var _current_direction: int
+var _can_input_direction: bool = true
+
+func _init(initial_direction: int):
+	_current_direction = initial_direction
+
+func get_current_length() -> int:
+	return _current_length
+
+func set_current_length(l: int) -> void:
+	_current_length = l
+
+func get_potential_length() -> int:
+	return _potential_length
+
+func set_potential_length(l: int) -> void:
+	_potential_length = l
+
+func get_current_direction() -> int:
+	return _current_direction
+
+func set_current_direction(d: int) -> void:
+	_current_direction = d
+
+func get_can_input_direction() -> bool:
+	return _can_input_direction
+
+func set_can_input_direction(can_input: bool) -> void:
+	_can_input_direction = can_input
