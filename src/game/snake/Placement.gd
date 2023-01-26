b8cf76d3a24f4bbece381120abb4ad1a2bdd0393
Tale152 <alessandro.talmi@gmail.com>
2023-01-26 15:33:35 +0100

    Refactored Placement.gd

--- src/game/snake/Placement.gd
+++ src/game/snake/Placement.gd
@@ -1,9 +1,23 @@
-class Placement extends Reference:
-	var coordinates
-	var next_direction
-	var previous_direction
-	
-	func _init(cord, next_dir, previous_dir):
-		coordinates = cord
-		next_direction = next_dir
-		previous_direction = previous_dir
+class_name Placement extends Reference
+
+var _coordinates: ImmutablePoint
+var _next_direction: int
+var _previous_direction: int
+
+func _init(
+	cordinates: ImmutablePoint,
+	next_direction: int,
+	previous_direction: int
+):
+	_coordinates = cordinates
+	_next_direction = next_direction
+	_previous_direction = previous_direction
+
+func get_coordinates() -> ImmutablePoint:
+	return _coordinates
+
+func get_next_direction() -> int:
+	return _next_direction
+
+func get_previous_direction() -> int:
+	return _previous_direction
