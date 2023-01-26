e7c3f1f2cb5987b1ca89c86573072b250df6c999
Tale152 <alessandro.talmi@gmail.com>
2023-01-26 15:44:57 +0100

    Refactored Head.gd

--- src/game/snake/Head.gd
+++ src/game/snake/Head.gd
@@ -1,31 +1,46 @@
 extends Area2D
 
-var placement
-var _visual_parameters
-var _sprite
+const Game = preload("res://src/game/Game.gd")
 
-func initialize(starting_placement, visual_parameters):
-	_visual_parameters = visual_parameters
-	move_to(starting_placement)
+var _placement: Placement
+var _visual_parameters: VisualParameters
+var _px: int
+var _stage_description: StageDescription
+var _sprite: AnimatedSprite
+
+func initialize(game: Game):
+	_visual_parameters = game.get_visual_parameters()
+	_stage_description = game.get_stage_description()
+	_px = _visual_parameters.get_cell_pixels_size()
+	move_to(Placement.new(
+		_stage_description.get_snake_spawn_point(),
+		_stage_description.get_snake_initial_direction(),
+		-1
+	))
 	
-func move_to(new_placement):
-	placement = new_placement
-	position = placement.coordinates
+func move_to(new_placement: Placement):
+	_placement = new_placement
+	position = Vector2(
+		new_placement.get_coordinates().get_x() * _px,
+		new_placement.get_coordinates().get_y() * _px
+	)
 	_set_sprite()
+
+func play_sprite_animation(speed_scale: float):
+	_sprite.speed_scale = speed_scale
+	_sprite.play()
+
+func get_placement() -> Placement:
+	return _placement
 
 func _set_sprite():
 	var sprite_tmp = _visual_parameters \
 		.get_head_sprite(
-			placement.next_direction,
-			placement.previous_direction == null # is tail?
+			_placement.get_next_direction(),
+			_placement.get_previous_direction() == -1 # is tail?
 		)
-	var px = _visual_parameters.get_cell_pixels_size()
-	sprite_tmp.set_offset(Vector2(px / 2, px / 2))
+	sprite_tmp.set_offset(Vector2(_px / 2, _px / 2))
 	if _sprite != null:
 		remove_child(_sprite)
 	_sprite = sprite_tmp
 	self.add_child(_sprite)
-
-func play_sprite_animation(speed_scale):
-	_sprite.speed_scale = speed_scale
-	_sprite.play()
