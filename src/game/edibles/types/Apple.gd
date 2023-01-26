faa7c3354441392f0e17ab9e040f21f4ab3bac3e
Tale152 <alessandro.talmi@gmail.com>
2023-01-26 17:26:53 +0100

    Changed arguments name to avoid unused argument warning

--- src/game/edibles/types/Apple.gd
+++ src/game/edibles/types/Apple.gd
@@ -1,7 +1,12 @@
 class_name Apple extends Reference
 
-func execute(placement, rules, snake, game):
-	snake.get_properties().set_potential_length(
-		snake.get_properties().get_potential_length() + 1
+func execute(
+	_placement,
+	_rules,
+	_snake,
+	_game	
+) -> bool:
+	_snake.get_properties().set_potential_length(
+		_snake.get_properties().get_potential_length() + 1
 	)
 	return true
