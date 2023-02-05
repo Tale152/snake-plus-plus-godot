7eaba036bf5549c0472b7aabe93191e1658b7faf
Tale152 <alessandro.talmi@gmail.com>
2023-02-05 20:02:56 +0100

    Changed swipe controls handling

--- src/game/player_input/SwipeMovementInput.gd
+++ src/game/player_input/SwipeMovementInput.gd
@@ -1,11 +1,15 @@
 class_name SwipeMovementInput extends Reference
 
-const SWIPE_CONTROLS_SENSITIVITY = 5
+const SWIPE_CONTROLS_SENSITIVITY = 20
 
 static func get_input_direction(event: InputEventScreenDrag) -> int:
 	var swipe = event.relative
-	if swipe.y < -SWIPE_CONTROLS_SENSITIVITY: return Directions.get_up()
-	elif swipe.y > SWIPE_CONTROLS_SENSITIVITY: return Directions.get_down()
-	elif swipe.x < -SWIPE_CONTROLS_SENSITIVITY: return Directions.get_left()
-	elif swipe.x > SWIPE_CONTROLS_SENSITIVITY: return Directions.get_right()
-	else: return -1
+	var abs_x = abs(swipe.x)
+	var abs_y = abs(swipe.y)
+	if abs_y > abs_x:
+		if abs_y > SWIPE_CONTROLS_SENSITIVITY:
+			return Directions.get_up() if swipe.y < 0 else Directions.get_down()
+	else:
+		if abs_x > SWIPE_CONTROLS_SENSITIVITY:
+			return Directions.get_left() if swipe.x < 0 else Directions.get_right()
+	return -1
