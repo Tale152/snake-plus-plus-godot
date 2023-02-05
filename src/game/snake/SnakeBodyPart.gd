ef716ad5a08a86b4a09311eabcbb7bc131464d03
Tale152 <alessandro.talmi@gmail.com>
2023-02-05 01:03:12 +0100

    Refactored SnakeBodyPart, deleted SnakeSpriteCalculator

--- src/game/snake/SnakeBodyPart.gd
+++ src/game/snake/SnakeBodyPart.gd
@@ -16,7 +16,7 @@ func _init(starting_placement: Placement, snake, game):
 	_px = _visual_parameters.get_cell_pixels_size()
 	_offset = _visual_parameters.get_game_pixels_offset()
 	update_position_on_screen()
-	update_sprite()
+	_calculate_and_render_new_sprite()
 
 func get_placement() -> Placement:
 	return _placement
@@ -30,10 +30,11 @@ func update_position_on_screen() -> void:
 	)
 
 func update_sprite() -> void:
-	var new_sprite: AnimatedSprite = SnakeSpriteCalculator \
-		.get_body_or_tail_sprite(_snake, _visual_parameters, _placement)
-	if _sprite != null:
-		remove_child(_sprite)
+	remove_child(_sprite)
+	_calculate_and_render_new_sprite()
+
+func _calculate_and_render_new_sprite() -> void:
+	var new_sprite: AnimatedSprite = _get_body_or_tail_sprite()
 	_sprite = new_sprite
 	self.add_child(_sprite)
 
@@ -44,5 +45,25 @@ func play_sprite_animation(speed_scale: float) -> void:
 func stop_sprite_animation() -> void:
 	_sprite.stop()
 	
-func on_snake_head_collision():
+func on_snake_head_collision() -> void:
 	_game.set_game_over(true)
+
+func _get_body_or_tail_sprite() -> AnimatedSprite:
+	if(_placement.get_previous_direction() == -1):
+		# is tail
+		if _snake.get_properties().get_current_length() > 2:
+			# base tail case: snake's length > 2
+			return _visual_parameters.get_tail_sprite(
+				_placement.get_next_direction()
+			)
+		else:
+			# special tail case: snake's length == 2
+			return _visual_parameters.get_tail_sprite(
+				_snake.get_head().get_placement().get_next_direction()
+			)
+	else:
+		# is body
+		return _visual_parameters.get_body_sprite(
+			_placement.get_next_direction(),
+			_placement.get_previous_direction()
+		)
