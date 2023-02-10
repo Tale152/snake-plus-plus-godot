448d1a5c74db620cd63aa4166df53b28adaceb6d
Tale152 <alessandro.talmi@gmail.com>
2023-02-10 18:13:03 +0100

    Updated Apple's effect

--- src/game/edibles/types/Apple.gd
+++ src/game/edibles/types/Apple.gd
@@ -1,5 +1,7 @@
 class_name Apple extends Reference
 
+const APPLE_POINTS = 10
+
 func execute(
 	_placement,
 	_rules,
@@ -8,5 +10,9 @@ func execute(
 ) -> bool:
 	_snake.get_properties().set_potential_length(
 		_snake.get_properties().get_potential_length() + 1
+	)
+	var player: Player = _game.get_player()
+	player.set_points(
+		player.get_points() + APPLE_POINTS * player.get_multiplier()
 	)
 	return true
