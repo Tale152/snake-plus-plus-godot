2d0326f51f4c97bb36dd3af9a49db3fc9a7fd23c
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-02-12 14:39:47 +0100

    Changed DevMenu to offer stages

---
+++ src/menu/DevMenuStages.gd
@@ -0,0 +1,172 @@
+class_name DevMenuStages extends Reference
+
+static func TOP_HUD_HEIGHT_PX() -> int:
+	return int(OS.window_size.y * 0.1)
+
+static func complete_builders(
+	stage_description_builder: StageDescriptionBuilder,
+	visual_parameters_builder: VisualParametersBuilder,
+	edibles_skin_path: String,
+	stage_index: int
+) -> void:
+	match stage_index:
+		0: _complete_0(stage_description_builder, visual_parameters_builder, edibles_skin_path)
+		1: _complete_1(stage_description_builder, visual_parameters_builder, edibles_skin_path)
+		2: _complete_2(stage_description_builder, visual_parameters_builder, edibles_skin_path)
+
+static func _complete_0(
+	stage_description_builder: StageDescriptionBuilder,
+	visual_parameters_builder: VisualParametersBuilder,
+	edibles_skin_path: String
+) -> void:
+	var width = 16
+	var heigh = 16
+	stage_description_builder \
+		.set_field_size(FieldSize.new(width, heigh)) \
+		.set_snake_spawn_point(ImmutablePoint.new(0,0)) \
+		.set_snake_initial_direction(Directions.get_right()) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.APPLE()) \
+				.set_max_instances(7) \
+				.set_life_spawn(-1) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(1) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.GAIN_COIN()) \
+				.set_max_instances(3) \
+				.set_life_spawn(5) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.2) \
+				.build()
+		)
+	visual_parameters_builder \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.APPLE())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.GAIN_COIN()))
+	_set_px(visual_parameters_builder, width, heigh)
+
+static func _complete_1(
+	stage_description_builder: StageDescriptionBuilder,
+	visual_parameters_builder: VisualParametersBuilder,
+	edibles_skin_path: String
+) -> void:
+	var width = 15
+	var heigh = 11
+	stage_description_builder \
+		.set_field_size(FieldSize.new(width, heigh)) \
+		.set_snake_spawn_point(ImmutablePoint.new(7,5)) \
+		.set_snake_initial_direction(Directions.get_down()) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.APPLE()) \
+				.set_max_instances(5) \
+				.set_life_spawn(-1) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.9) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.BAD_APPLE()) \
+				.set_max_instances(3) \
+				.set_life_spawn(-1) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.6) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.GAIN_COIN()) \
+				.set_max_instances(3) \
+				.set_life_spawn(3) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.2) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.LOSS_COIN()) \
+				.set_max_instances(3) \
+				.set_life_spawn(3) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.4) \
+				.build()
+		)
+	visual_parameters_builder \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.APPLE())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.BAD_APPLE())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.GAIN_COIN())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.LOSS_COIN()))
+	_set_px(visual_parameters_builder, width, heigh)
+
+static func _complete_2(
+	stage_description_builder: StageDescriptionBuilder,
+	visual_parameters_builder: VisualParametersBuilder,
+	edibles_skin_path: String
+) -> void:
+	var width = 15
+	var heigh = 11
+	stage_description_builder \
+		.set_field_size(FieldSize.new(width, heigh)) \
+		.set_snake_spawn_point(ImmutablePoint.new(7,6)) \
+		.set_snake_initial_direction(Directions.get_down()) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.APPLE()) \
+				.set_max_instances(5) \
+				.set_life_spawn(5) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.9) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.BAD_APPLE()) \
+				.set_max_instances(3) \
+				.set_life_spawn(10) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.6) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.GAIN_COIN()) \
+				.set_max_instances(3) \
+				.set_life_spawn(3) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.2) \
+				.build()
+		) \
+		.add_edible_rules(
+			EdibleRulesBuiler.new() \
+				.set_type(EdibleTypes.LOSS_COIN()) \
+				.set_max_instances(3) \
+				.set_life_spawn(3) \
+				.set_spawn_locations([]) \
+				.set_spawn_probability(0.4) \
+				.build()
+		)
+	visual_parameters_builder \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.APPLE())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.BAD_APPLE())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.GAIN_COIN())) \
+		.add_edible_sprite(EdibleSprite.new(edibles_skin_path, EdibleTypes.LOSS_COIN()))
+	_set_px(visual_parameters_builder, width, heigh)
+
+static func _set_px(
+	visual_parameters_builder: VisualParametersBuilder,
+	width: int,
+	height: int
+) -> void:
+	var width_px = int(OS.window_size.x / width)
+	var heigh_px = int((OS.window_size.y - TOP_HUD_HEIGHT_PX())/ height)
+	var cell_px_size = width_px if width_px < heigh_px else heigh_px
+	visual_parameters_builder \
+		.set_cell_pixels_size(cell_px_size) \
+		.set_game_pixels_offset(Vector2(
+			(OS.window_size.x - width * cell_px_size) / 2,
+			TOP_HUD_HEIGHT_PX()
+		))
--- src/menu/Menu.gd
+++ src/menu/Menu.gd
@@ -1,14 +1,11 @@
 extends Control
 
-var SNAKE_SPAWN_POINT = ImmutablePoint.new(0,0)
-var SNAKE_INITIAL_DIRECTION = Directions.get_right()
-var TOP_HUD_HEIGHT_PX: int = OS.window_size.y * 0.1
-
 var _main
 
 func _ready():
 	$SpeedContainer/SpeedItemList.select(2)
 	$SkinContainer/SkinItemList.select(0)
+	$StageContainer/StageItemList.select(0)
 
 func set_main(main) -> void:
 	_main = main
@@ -21,48 +18,23 @@ func _on_PlayButton_pressed():
 		2: description_builder.set_snake_speedup_factor(0.985).set_snake_base_delta_seconds(0.4)
 		3: description_builder.set_snake_speedup_factor(0.98).set_snake_base_delta_seconds(0.3)
 		4: description_builder.set_snake_speedup_factor(0.975).set_snake_base_delta_seconds(0.2)
-	description_builder \
-		.set_field_size(FieldSize.new(int($FieldContainer/WidthTextEdit.text), int($FieldContainer/HeightTextEdit.text))) \
-		.set_snake_spawn_point(SNAKE_SPAWN_POINT) \
-		.set_snake_initial_direction(Directions.get_right())
-	if $AppleContainer/AppleCheckBox.pressed:
-		var apple_rules = EdibleRulesBuiler.new() \
-			.set_max_instances(int($AppleContainer/AppleNumberTextEdit.text)) \
-			.set_spawn_locations([]) \
-			.set_life_spawn(-1 if !$AppleContainer/AppleExpiresAfterCheckBox.pressed else 5) \
-			.set_spawn_probability(float($AppleContainer/AppleProbabilityTextEdit.text)) \
-			.set_type(EdibleTypes.APPLE()) \
-			.build()
-		description_builder.add_edible_rules(apple_rules)
-	if $BadAppleContainer/BadAppleCheckBox.pressed:
-		var bad_apple_rules = EdibleRulesBuiler.new() \
-			.set_max_instances(int($BadAppleContainer/BadAppleNumberTextEdit.text)) \
-			.set_spawn_locations([]) \
-			.set_life_spawn(-1 if !$BadAppleContainer/BadAppleExpiresAfterCheckBox.pressed else 5) \
-			.set_spawn_probability(float($BadAppleContainer/BadAppleProbabilityTextEdit.text)) \
-			.set_type(EdibleTypes.BAD_APPLE()) \
-			.build()
-		description_builder.add_edible_rules(bad_apple_rules)
-	var description: StageDescription = description_builder.build()
-	if description != null:
-		var width_px = int(OS.window_size.x / int($FieldContainer/WidthTextEdit.text))
-		var heigh_px = int((OS.window_size.y - TOP_HUD_HEIGHT_PX)/ int($FieldContainer/HeightTextEdit.text))
-		var cell_px_size = width_px if width_px < heigh_px else heigh_px
-		var selected_skin = str(
-			"res://assets/skins/", $SkinContainer/SkinItemList.get_item_text($SkinContainer/SkinItemList.get_selected_items()[0])
-		)
-		var visual_parameters_builder = VisualParametersBuilder.new() \
-			.set_cell_pixels_size(cell_px_size) \
-			.set_game_pixels_offset(Vector2(
-				(OS.window_size.x - int($FieldContainer/WidthTextEdit.text) * cell_px_size) / 2,
-				TOP_HUD_HEIGHT_PX
-			)) \
-			.set_snake_skin_path(str(selected_skin, "/snake")) \
-			.set_field_elements_skin_path(str(selected_skin, "/field"))
-		if $AppleContainer/AppleCheckBox.pressed:
-			visual_parameters_builder.add_edible_sprite(EdibleSprite.new(str(selected_skin, "/edibles"), "Apple"))
-		if $BadAppleContainer/BadAppleCheckBox.pressed:
-			visual_parameters_builder.add_edible_sprite(EdibleSprite.new(str(selected_skin, "/edibles"), "BadApple"))
-		_main.play(description, visual_parameters_builder.build())
-	else:
-		$WrongInputAcceptDialog.popup()
+	var selected_skin = str(
+		"res://assets/skins/", $SkinContainer/SkinItemList.get_item_text($SkinContainer/SkinItemList.get_selected_items()[0])
+	)
+	var visual_parameters_builder = VisualParametersBuilder.new()
+	visual_parameters_builder \
+		.set_snake_skin_path(str(selected_skin, "/snake")) \
+		.set_field_elements_skin_path(str(selected_skin, "/field"))
+	var selected_stage: int
+	match $StageContainer/StageItemList.get_selected_items()[0]:
+		0: selected_stage = 0
+		1: selected_stage = 1
+		2: selected_stage = 2
+	
+	DevMenuStages.complete_builders(
+		description_builder,
+		visual_parameters_builder,
+		str(selected_skin, "/edibles"),
+		selected_stage
+	)
+	_main.play(description_builder.build(), visual_parameters_builder.build())
