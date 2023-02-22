c8dce905e333b30f1f86ffc1af011c566d2ff664
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-02-22 13:51:47 +0100

    Added function to DictionaryUtil

--- src/stage_parser/util/DictionaryUtil.gd
+++ src/stage_parser/util/DictionaryUtil.gd
@@ -1,4 +1,7 @@
 class_name DictionaryUtil extends Reference
 
-static func contains(d: Dictionary, value: String, type: int):
+static func contains(d: Dictionary, value: String, type: int) -> bool:
 	return d.has(value) && typeof(d[value]) == type
+
+static func does_not_contain(d: Dictionary, value: String) -> bool:
+	return !d.has(value)
