29d38b57fa93fe79521c51b74dbb147438502fa0
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-02-22 14:14:08 +0100

    Commented JsonStageUpdater

--- src/stage_parser/JsonStageUpdater.gd
+++ src/stage_parser/JsonStageUpdater.gd
@@ -1,8 +1,10 @@
 class_name JsonStageUpdater extends Reference
 
+static func current_version() -> int: return 1
+
 static func update_to_latest_version(
 	stage_version: float,
 	stage: Dictionary
 ) -> Dictionary:
-	#TODO implement
+	# when new versions arise this will be used to adapt the stage
 	return stage
