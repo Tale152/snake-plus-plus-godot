c8e0c78c547d3ae023111a1174aa93585526f1fa
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-08 16:10:12 +0100

    Mini performance refactor

--- src/game/GameDirectionInputHandler.gd
+++ src/game/GameDirectionInputHandler.gd
@@ -21,14 +21,14 @@ func submit_input(
 	elif _is_direction_not_assigned(_next_next_direction):
 		if (
 			_compatible_movement(_next_direction, input_direction) &&
-			_can_register_next_next(movement_elapsed_seconds, current_snake_delta_seconds)
+			_can_register_future_movement(movement_elapsed_seconds, current_snake_delta_seconds, _NEXT_NEXT_DIRECTION_MOVEMENT_DELTA_TRESHOLD)
 		):
 			_next_next_direction = input_direction
 			return true
 	elif _is_direction_not_assigned(_next_next_next_direction):
 		if (
 			_compatible_movement(_next_next_direction, input_direction) &&
-			_can_register_next_next_next(movement_elapsed_seconds, current_snake_delta_seconds)
+			_can_register_future_movement(movement_elapsed_seconds, current_snake_delta_seconds, _NEXT_NEXT_NEXT_DIRECTION_MOVEMENT_DELTA_TRESHOLD)
 		):
 			_next_next_next_direction = input_direction
 			return true
@@ -61,23 +61,3 @@ func _can_register_future_movement(
 	treshold_delta_multiplier: float
 ) -> bool:
 	return movement_elapsed_seconds > (current_snake_delta_seconds * treshold_delta_multiplier)
-
-func _can_register_next_next(
-	movement_elapsed_seconds: float,
-	current_snake_delta_seconds: float
-) -> bool:
-	return _can_register_future_movement(
-		movement_elapsed_seconds,
-		current_snake_delta_seconds,
-		_NEXT_NEXT_DIRECTION_MOVEMENT_DELTA_TRESHOLD
-	)
-
-func _can_register_next_next_next(
-	movement_elapsed_seconds: float,
-	current_snake_delta_seconds: float
-) -> bool:
-	return _can_register_future_movement(
-		movement_elapsed_seconds,
-		current_snake_delta_seconds,
-		_NEXT_NEXT_NEXT_DIRECTION_MOVEMENT_DELTA_TRESHOLD
-	)
