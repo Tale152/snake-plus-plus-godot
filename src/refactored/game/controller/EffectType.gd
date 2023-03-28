57b56cc038a0ec9fa7e01ee577665f5cedf524d3
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-28 15:54:36 +0200

    Created EffectType and PerkType

---
+++ src/refactored/game/controller/EffectType.gd
@@ -0,0 +1,5 @@
+class_name EffectType
+# intended to be used and Enum that can be exported easily
+
+static func ORANGE() -> int: return PerkType.ORANGE()
+#TODO extend
---
+++ src/refactored/game/controller/PerkType.gd
@@ -0,0 +1,16 @@
+enum PerkTypeEnum {
+	APPLE,
+	LEMON,
+	CHERRY,
+	ORANGE
+}
+#TODO extend
+
+class_name PerkType
+# intended to be used and Enum that can be exported easily
+
+static func APPLE() -> int: return PerkTypeEnum.APPLE
+static func LEMON() -> int: return PerkTypeEnum.LEMON
+static func CHERRY() -> int: return PerkTypeEnum.CHERRY
+static func ORANGE() -> int: return PerkTypeEnum.ORANGE
+#TODO extend
