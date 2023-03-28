1c13d7408e0fe86c096b224ace1f96885b18ad9c
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-28 16:47:55 +0200

    Created EquippedEffectsContainer

---
+++ src/refactored/game/model/EquippedEffectsContainer.gd
@@ -0,0 +1,29 @@
+class_name EquippedEffectsContainer extends Reference
+
+var _equipped_effects: Array = []
+
+func equip_effect(
+	effect: EquippedEffectTODO, 
+	snake_properties: SnakePropertiesTODO
+) -> void:
+	for i in range(0, _equipped_effects.size()):
+		if _equipped_effects[i].get_type() == effect.get_type():
+			_equipped_effects[i] = effect
+			return
+	_equipped_effects.push_back(effect)
+	effect.apply_effect(snake_properties)
+
+func revoke_effect(
+	effect_type: int,
+	snake_properties: SnakePropertiesTODO
+) -> void:
+	for i in range(0, _equipped_effects.size()):
+		if _equipped_effects[i].get_type() == effect_type:
+			_equipped_effects[i].revoke_effect(snake_properties)
+			_equipped_effects.remove(i)
+			return
+
+func get_equipped_effects() -> Array:
+	var res = []
+	for e in _equipped_effects: res.push_back(e)
+	return res
