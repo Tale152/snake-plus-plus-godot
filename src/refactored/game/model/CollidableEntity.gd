013236f81210b7bed3d02cee0b37f1ca1360d75a
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-28 15:42:53 +0200

    Created CollidableEntity and ExpirableEntity

---
+++ src/refactored/game/model/CollidableEntity.gd
@@ -0,0 +1,17 @@
+class_name CollidableEntity extends Reference
+
+var _coordinates: Coordinates
+var _collision_strategy: CollisionStrategy
+
+func _init(
+	coordinates: Coordinates,
+	collision_strategy: CollisionStrategy
+):
+	_coordinates = coordinates
+	_collision_strategy = collision_strategy
+
+func get_coordinates() -> Coordinates:
+	return _coordinates
+
+func collide(snake_properties: SnakePropertiesTODO) -> CollisionResult:
+	return _collision_strategy.execute(snake_properties)
---
+++ src/refactored/game/model/ExpirableEntity.gd
@@ -0,0 +1,28 @@
+class_name ExpirableEntity extends Reference
+
+var _elapsed_seconds: float
+var _lifespan_seconds: float
+
+func _init(lifespan_seconds: float = -1.0):
+	_lifespan_seconds = lifespan_seconds
+	_elapsed_seconds = -1.0 if _lifespan_seconds == -1.0 else 0.0
+
+func tick(delta_seconds: float) -> void:
+	if _lifespan_seconds != -1.0:
+		_elapsed_seconds += delta_seconds
+
+func can_expire() -> bool:
+	return _lifespan_seconds != -1.0
+
+func has_expired() -> bool:
+	if _lifespan_seconds == -1.0:
+		# keep performances in mind, do not use can_expire
+		return false
+	return _elapsed_seconds >= _lifespan_seconds
+
+func get_lifespan_percentage() -> float:
+	if _lifespan_seconds == -1.0:
+		# keep performances in mind, do not use can_expire
+		return -1.0
+	var percentage: float = _elapsed_seconds / _lifespan_seconds
+	return 1.0 if percentage > 1.0 else percentage
