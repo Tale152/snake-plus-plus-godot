f53bfb3cb88f1cbc275a36fe75e72dcdc2c62920
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-28 15:17:51 +0200

    Created CollisionStrategy

---
+++ src/refactored/game/model/CollisionStrategy.gd
@@ -0,0 +1,9 @@
+class_name CollisionStrategy extends Reference
+# is ment to be used as an Interface
+
+func execute(snake_properties: SnakePropertiesTODO) -> CollisionResult:
+	push_error(
+		"You called the execute function of CollisionStrategy, " + 
+		"which is ment to be used as an interface"
+	)
+	return null
