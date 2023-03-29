a7e70cafacb6ee0941c22914d5c76347de42295e
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-29 10:08:18 +0200

    Created Direction

---
+++ src/refactored/game/controller/Direction.gd
@@ -0,0 +1,13 @@
+enum DirectionEnum {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT
+}
+
+class_name Direction
+
+static func UP() -> int: return DirectionEnum.UP
+static func RIGHT() -> int: return DirectionEnum.RIGHT
+static func DOWN() -> int: return DirectionEnum.DOWN
+static func LEFT() -> int: return DirectionEnum.LEFT
