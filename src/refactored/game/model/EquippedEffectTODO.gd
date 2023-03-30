1dfd23390ccf08eb9e4a301e8c3412cb22fc831e
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-03-30 10:13:09 +0200

    Removed EffectStrategy

--- src/refactored/game/model/EquippedEffectTODO.gd
+++ src/refactored/game/model/EquippedEffectTODO.gd
@@ -1,16 +1,13 @@
 class_name EquippedEffectTODO extends Reference
-
+# meant to be used as abstract class 
 var _type: int
-var _effect_strategy: EffectStrategy
 var _expire_timer: ExpireTimer
 
 func _init(
 	effect_type: int,
-	effect_strategy: EffectStrategy,
 	lifespan_seconds: float
 ):
 	_type = effect_type
-	_effect_strategy = effect_strategy
 	_expire_timer = ExpireTimer.new(lifespan_seconds)
 
 func get_type() -> int: return _type
@@ -18,7 +15,9 @@ func get_type() -> int: return _type
 func get_expire_timer() -> ExpireTimer: return _expire_timer
 
 func apply_effect(snake_properties: SnakePropertiesTODO) -> void:
-	_effect_strategy.execute_apply_effect(snake_properties)
+	# override on extending class
+	pass
 
 func revoke_effect(snake_properties: SnakePropertiesTODO) -> void:
-	_effect_strategy.execute_revoke_effect(snake_properties)
+	# override on extending class
+	pass
