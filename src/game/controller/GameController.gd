b72160149d12f5270963cb0581063f248423fe08
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-04-01 20:21:50 +0200

    handled equipped effects tick

--- src/game/controller/GameController.gd
+++ src/game/controller/GameController.gd
@@ -5,11 +5,16 @@ var _raw_material: ModelRawMaterial
 var _model: GameModel
 var _body_part_factory: SnakeBodyPartFactory
 var _perks_rules: Array
+var _elapsed_seconds: float
 
 func _init(parsed_stage: ParsedStage, effects_lifespan_seconds: float):
 	_raw_material = ModelRawMaterial.new(parsed_stage, effects_lifespan_seconds)
 	_body_part_factory = _raw_material.get_body_part_factory()
 	_perks_rules = _raw_material.get_perks_rules()
+
+func tick(delta_seconds: float) -> void:
+	_elapsed_seconds += delta_seconds
+	_handle_equipped_effects_tick(delta_seconds)
 
 func start_new_game() -> void:
 	var head: SnakeBodyPart = _body_part_factory.create_new(
@@ -28,3 +33,12 @@ func start_new_game() -> void:
 		walls,
 		_raw_material.get_snake_initial_direction()
 	)
+	_elapsed_seconds = 0.0
+
+func _handle_equipped_effects_tick(delta_seconds) -> void:
+	var container: EquippedEffectsContainer = _model.get_equipped_effects_container()
+	var snake_properties: SnakeProperties = _model._snake_properties()
+	for effect in container.get_equipped_effects():
+		effect.get_expire_timer().tick(delta_seconds)
+		if effect.get_expire_timer().has_expired():
+			container.revoke_effect(effect, snake_properties)
