6d799da1ec09ff00db5dca82583aa83265c2f990
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-04-01 17:22:11 +0200

    Expanded EffectTypes

--- src/game/controller/EffectType.gd
+++ src/game/controller/EffectType.gd
@@ -2,4 +2,8 @@ class_name EffectType
 # intended to be used and Enum that can be exported easily
 
 static func ORANGE() -> int: return PerkType.ORANGE()
-#TODO extend
+static func CHILI() -> int: return PerkType.CHILI()
+static func STAR() -> int: return PerkType.STAR()
+static func BANANA() -> int: return PerkType.BANANA()
+static func AVOCADO() -> int: return PerkType.AVOCADO()
+
