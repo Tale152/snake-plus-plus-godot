09d7a96ad9cb91b06f7a4a13205df21b69b60967
Tale152 <alessandro.talmi@gmail.com>
2023-04-11 15:18:44 +0200

    Added utility functions to Direction

--- src/game/controller/Direction.gd
+++ src/game/controller/Direction.gd
@@ -12,3 +12,14 @@ static func UP() -> int: return DirectionEnum.UP
 static func RIGHT() -> int: return DirectionEnum.RIGHT
 static func DOWN() -> int: return DirectionEnum.DOWN
 static func LEFT() -> int: return DirectionEnum.LEFT
+
+static func get_directions() -> Array:
+	return [DirectionEnum.UP, DirectionEnum.RIGHT, DirectionEnum.DOWN, DirectionEnum.LEFT]
+
+static func are_opposite(dir1: int, dir2: int) -> bool:
+	match dir1:
+		DirectionEnum.UP: return dir2 == DirectionEnum.DOWN
+		DirectionEnum.RIGHT: return dir2 == DirectionEnum.LEFT
+		DirectionEnum.DOWN: return dir2 == DirectionEnum.UP
+		DirectionEnum.LEFT: return dir2 == DirectionEnum.RIGHT
+	return false
