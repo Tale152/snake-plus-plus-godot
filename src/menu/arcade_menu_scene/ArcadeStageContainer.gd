76739d90fa1929d28f0279bddc14e0bce4079ece
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-05-17 00:15:32 +0200

    Scaled text in ArcadeStageContainer

--- src/menu/arcade_menu_scene/ArcadeStageContainer.gd
+++ src/menu/arcade_menu_scene/ArcadeStageContainer.gd
@@ -1,8 +1,11 @@
 class_name ArcadeStageContainer extends Control
 
+var _StageNameFont = preload("res://src/menu/arcade_menu_scene/StageNameFont.tres")
+
 var _on_pressed_strategy: FuncRef
 var _data: ArcadeStageData
 
+const _DEFAULT_STAGE_NAME_FONT_SIZE: int = 22
 const _DEFAULT_MIN_X: int = 282
 const _DEFAULT_MIN_Y: int = 120
 
@@ -17,8 +20,12 @@ func initialize(
 		_DEFAULT_MIN_X * scale,
 		_DEFAULT_MIN_Y * scale
 	)
+	_StageNameFont.size = _get_int_font_size(_DEFAULT_STAGE_NAME_FONT_SIZE, scale)
 	_data = data
 	$Button.text = text
+
+func _get_int_font_size(default_value: int, scale: float) -> int:
+	return int(floor(default_value * scale))
 
 func _on_Button_pressed():
 	_on_pressed_strategy.call_func(_data)
