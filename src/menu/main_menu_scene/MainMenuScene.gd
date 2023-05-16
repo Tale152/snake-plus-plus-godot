4a0cc730cf127ac68d738861594f02fb17c54358
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-05-16 23:24:49 +0200

    Created SettingsMenuScene

--- src/menu/main_menu_scene/MainMenuScene.gd
+++ src/menu/main_menu_scene/MainMenuScene.gd
@@ -1,6 +1,7 @@
 class_name MainMenuScene extends Control
 
 const _MainMenuContent = preload("res://src/menu/main_menu_scene/MainMenuContent.tscn")
+const _SettingsMenuScene = preload("res://src/menu/settings_menu_scene/SettingsMenuScene.tscn")
 
 onready var _NavigationBar: NavigationBar = $MenuSceneControl.get_navigation_bar()
 
@@ -17,7 +18,8 @@ func _ready():
 	_NavigationBar.set_left_button_disabled(true)
 	_NavigationBar.set_title_label_text("")
 	_NavigationBar.set_right_button_visible(true, "settings")
-	_NavigationBar.set_right_button_disabled(true)
+	_NavigationBar.set_right_button_disabled(false)
+	_NavigationBar.set_on_right_button_pressed_strategy(funcref(self, "_go_to_settings"))
 	_main_menu_content.scale($MenuSceneControl.get_scaling())
 
 func initialize(main_scene_instance: Control) -> void:
@@ -25,3 +27,7 @@ func initialize(main_scene_instance: Control) -> void:
 	_main_scene_instance.clear()
 	_main_menu_content.initialize(main_scene_instance, self)
 	_main_scene_instance.add_child(self)
+
+func _go_to_settings() -> void:
+	var settings_menu_scene: SettingsMenuScene = _SettingsMenuScene.instance()
+	settings_menu_scene.initialize(_main_scene_instance, self)
