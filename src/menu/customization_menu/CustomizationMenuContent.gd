58e98b8776f752aa8465fa52359e017cf55059cf
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-05-18 01:57:13 +0200

    Created CustomizationMenuContent and PersistentCustomizationSettings

---
+++ src/menu/customization_menu/CustomizationMenuContent.gd
@@ -0,0 +1,39 @@
+class_name CustomizationMenuContent extends Control
+
+const _SKINS_ARRAY: Array = ["simple", "debug", "kawaii"]
+
+func _ready():
+	$SnakeOptionChooser.fill(
+		"Snake", 
+		_SKINS_ARRAY,
+		_get_skins_array_index(PersistentCustomizationSettings.get_snake_skin()),
+		funcref(self, "_change_snake")
+	)
+	$FieldOptionChooser.fill(
+		"Field", 
+		_SKINS_ARRAY,
+		_get_skins_array_index(PersistentCustomizationSettings.get_field_skin()),
+		funcref(self, "_change_field")
+	)
+	$PerksOptionChooser.fill(
+		"Perks", 
+		_SKINS_ARRAY,
+		_get_skins_array_index(PersistentCustomizationSettings.get_perks_skin()),
+		funcref(self, "_change_perks")
+	)
+
+func _change_snake(skin: String) -> void:
+	PersistentCustomizationSettings.set_snake_skin(skin)
+
+func _change_field(skin: String) -> void:
+	PersistentCustomizationSettings.set_field_skin(skin)
+
+func _change_perks(skin: String) -> void:
+	PersistentCustomizationSettings.set_perks_skin(skin)
+
+func _get_skins_array_index(skin: String) -> int:
+	var i = 0
+	for s in _SKINS_ARRAY:
+		if skin == s: return i
+		i += 1
+	return 0
---
+++ src/persistence/PersistentCustomizationSettings.gd
@@ -0,0 +1,56 @@
+extends Node
+
+const _CUSTOMIZATION_FILE_PATH: String = "user://customization_settings.json"
+const _SNAKE: String = "snake"
+const _FIELD: String = "field"
+const _PERKS: String = "perks"
+
+func _ready():
+	if not Persistence.exists(_CUSTOMIZATION_FILE_PATH):
+		Persistence.write(_CUSTOMIZATION_FILE_PATH, _get_default_customization())
+	else:
+		var has_changed: bool = false
+		var data: Dictionary = Persistence.read(_CUSTOMIZATION_FILE_PATH)
+		var changes: int = 0
+		changes += _fix_field(data, _SNAKE, "Simple")
+		changes += _fix_field(data, _FIELD, "Simple")
+		changes += _fix_field(data, _PERKS, "Simple")
+		if changes > 0:
+			Persistence.write(_CUSTOMIZATION_FILE_PATH, data)
+
+func _get_default_customization() -> Dictionary:
+	return {
+		_SNAKE: "Simple",
+		_FIELD: "Simple",
+		_PERKS: "Simple"
+	}
+
+func _fix_field(data: Dictionary, field_name: String, value: String) -> int:
+	var field = data[field_name]
+	if field == null:
+		data[field_name] = value
+		return 1
+	return 0
+
+func get_snake_skin() -> String:
+	return Persistence.read(_CUSTOMIZATION_FILE_PATH)[_SNAKE]
+
+func set_snake_skin(skin: String) -> void:
+	_set_string_value(_SNAKE, skin)
+
+func get_field_skin() -> String:
+	return Persistence.read(_CUSTOMIZATION_FILE_PATH)[_FIELD]
+
+func set_field_skin(skin: String) -> void:
+	_set_string_value(_FIELD, skin)
+
+func get_perks_skin() -> String:
+	return Persistence.read(_CUSTOMIZATION_FILE_PATH)[_PERKS]
+
+func set_perks_skin(skin: String) -> void:
+	_set_string_value(_PERKS, skin)
+
+func _set_string_value(field_name: String, value: String) -> void:
+	var data: Dictionary = Persistence.read(_CUSTOMIZATION_FILE_PATH)
+	data[field_name] = value
+	Persistence.write(_CUSTOMIZATION_FILE_PATH, data)
