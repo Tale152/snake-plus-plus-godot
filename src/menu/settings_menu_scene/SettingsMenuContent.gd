670bb66e05b6d790b74c70bba473b74fa82f651d
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2023-05-20 19:48:43 +0200

    Handled master volume bus volume

--- src/menu/settings_menu_scene/SettingsMenuContent.gd
+++ src/menu/settings_menu_scene/SettingsMenuContent.gd
@@ -9,6 +9,7 @@ func _ready():
 		_get_controls_array_index(PersistentUserSettings.get_controls()),
 		funcref(self, "_change_controls")
 	)
+	$MusicHSlider.value = PersistentUserSettings.get_master_bus_volume()
 
 func scale(scale: float) -> void:
 	$ControlsOptionChooser.scale_font(scale)
@@ -22,3 +23,11 @@ func _get_controls_array_index(controls: String) -> int:
 		if controls == c: return i
 		i += 1
 	return 0
+
+func _on_MusicHSlider_value_changed(value: int) -> void:
+	if value == -45:
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
+		AudioServer.set_bus_volume_db(0, value)
+	PersistentUserSettings.set_master_bus_volume(value)
--- src/persistence/PersistentUserSettings.gd
+++ src/persistence/PersistentUserSettings.gd
@@ -2,7 +2,7 @@ extends Node
 
 const _SETTINGS_FILE_PATH: String = "user://user_settings.json"
 const _CONTROLS: String = "controls"
-const _MASTER_BUS_VOLUME_PERCENTAGE = "master_volume_percentage"
+const _MASTER_BUS_VOLUME = "master_volume"
 
 func _ready():
 	if not Persistence.exists(_SETTINGS_FILE_PATH):
@@ -12,7 +12,7 @@ func _ready():
 		var data: Dictionary = Persistence.read(_SETTINGS_FILE_PATH)
 		var changes: int = 0
 		changes += Persistence.fix_field(data, _CONTROLS, "Swipe")
-		changes += Persistence.fix_field(data, _MASTER_BUS_VOLUME_PERCENTAGE, 1.0)
+		changes += Persistence.fix_field(data, _MASTER_BUS_VOLUME, 0)
 		if changes > 0:
 			Persistence.write(_SETTINGS_FILE_PATH, data)
 
@@ -24,16 +24,16 @@ func set_controls(controls: String) -> void:
 	data[_CONTROLS] = controls
 	Persistence.write(_SETTINGS_FILE_PATH, data)
 
-func get_master_bus_volume_percentage() -> float:
-	return Persistence.read(_SETTINGS_FILE_PATH)[_MASTER_BUS_VOLUME_PERCENTAGE]
+func get_master_bus_volume() -> int:
+	return Persistence.read(_SETTINGS_FILE_PATH)[_MASTER_BUS_VOLUME]
 
-func set_master_bus_volume_percentage(percentage: float) -> void:
+func set_master_bus_volume(value: int) -> void:
 	var data: Dictionary = Persistence.read(_SETTINGS_FILE_PATH)
-	data[_MASTER_BUS_VOLUME_PERCENTAGE] = percentage
+	data[_MASTER_BUS_VOLUME] = value
 	Persistence.write(_SETTINGS_FILE_PATH, data)
 
 func _get_default_settings() -> Dictionary:
 	return {
 		_CONTROLS: "Swipe",
-		_MASTER_BUS_VOLUME_PERCENTAGE: 1.0
+		_MASTER_BUS_VOLUME: 0
 	}
