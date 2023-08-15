f45fb3fe9c7b5ed03c89a9548e242bac1e534ec2
Tale152 <alessandro.talmi@studio.unibo.it>
2023-08-15 12:20:22 +0200

    Refactor: created ArcadeRecordHelper to make PlayMenuScene simpler

---
+++ src/menu/play_menu_scene/ArcadeRecordHelper.gd
@@ -0,0 +1,86 @@
+class_name ArcadeRecordHelper extends Reference
+
+var _stages_data: Dictionary
+
+func _init(stages_data: Dictionary):
+	_stages_data = stages_data
+
+func _save_new_record(
+	uuid: String, stage_result: StageResult
+) -> void:
+	var difficulty: String = PersistentPlaySettings.get_difficulty()
+	var current_record: ArcadeRecord = _stages_data[uuid]
+	if current_record == null:
+		var new_record: ArcadeRecord
+		if difficulty == PersistentPlaySettings.NOOB:
+			new_record = ArcadeRecord.new(stage_result, stage_result, null, null, null, null)
+		elif difficulty == PersistentPlaySettings.REGULAR:
+			new_record = ArcadeRecord.new(null, null, stage_result, stage_result, null, null)
+		else:
+			new_record = ArcadeRecord.new(null, null, null, null, stage_result, stage_result)
+		PersistentArcadeStagesData.set_new_record(
+			uuid,
+			new_record
+		)
+		return
+	var current_length_record: StageResult = current_record.get_length_record(difficulty)
+	var current_score_record: StageResult = current_record.get_score_record(difficulty)
+	
+	var new_length_record = null
+	if current_length_record == null:
+		new_length_record = stage_result
+	elif stage_result.get_length() > current_length_record.get_length():
+		new_length_record = stage_result
+	elif stage_result.get_length() == current_length_record.get_length():
+		if stage_result.get_score() > current_length_record.get_score():
+			new_length_record = stage_result
+		elif stage_result.get_score() == current_length_record.get_score():
+			if stage_result.get_time() < current_length_record.get_time():
+				new_length_record = stage_result
+	
+	var new_score_record = null
+	if current_score_record == null:
+		new_score_record = stage_result
+	elif stage_result.get_score() > current_score_record.get_score():
+		new_score_record = stage_result
+	elif stage_result.get_score() == current_score_record.get_score():
+		if stage_result.get_length() > current_score_record.get_length():
+			new_score_record = stage_result
+		elif stage_result.get_length() == current_score_record.get_length():
+			if stage_result.get_time() < current_score_record.get_time():
+				new_score_record = stage_result
+	
+	if new_length_record != null || new_score_record != null:
+		if new_length_record != null:
+			current_length_record = new_length_record
+		if new_score_record != null:
+			current_score_record = new_score_record
+		var new_record: ArcadeRecord
+		if difficulty == PersistentPlaySettings.NOOB:
+			new_record = ArcadeRecord.new(
+				current_score_record,
+				current_length_record,
+				current_record.get_score_record(PersistentPlaySettings.REGULAR),
+				current_record.get_length_record(PersistentPlaySettings.REGULAR),
+				current_record.get_score_record(PersistentPlaySettings.PRO),
+				current_record.get_length_record(PersistentPlaySettings.PRO)
+			)
+		elif difficulty == PersistentPlaySettings.REGULAR:
+			new_record = ArcadeRecord.new(
+				current_record.get_score_record(PersistentPlaySettings.NOOB),
+				current_record.get_length_record(PersistentPlaySettings.NOOB),
+				current_score_record,
+				current_length_record,
+				current_record.get_score_record(PersistentPlaySettings.PRO),
+				current_record.get_length_record(PersistentPlaySettings.PRO)
+			)
+		else:
+			new_record = ArcadeRecord.new(
+				current_record.get_score_record(PersistentPlaySettings.NOOB),
+				current_record.get_length_record(PersistentPlaySettings.NOOB),
+				current_record.get_score_record(PersistentPlaySettings.REGULAR),
+				current_record.get_length_record(PersistentPlaySettings.REGULAR),
+				current_score_record,
+				current_length_record
+			)
+		PersistentArcadeStagesData.set_new_record(uuid, new_record)
--- src/menu/play_menu_scene/PlayMenuScene.gd
+++ src/menu/play_menu_scene/PlayMenuScene.gd
@@ -11,6 +11,7 @@ var _stage_prelude: StagePrelude = _StagePrelude.instance()
 var _main_scene_instance: Control
 var _main_menu_scene
 var _stages_data: Dictionary
+var _arcade_record_helper: ArcadeRecordHelper
 
 func _ready():
 	_NavigationBar.set_title_label_text(TranslationsManager.get_localized_string(
@@ -27,6 +28,7 @@ func _ready():
 	_play_menu_content.anchor_bottom = 1
 	_play_menu_content.visible = true
 	_stages_data = PersistentArcadeStagesData.get_stages()
+	_arcade_record_helper = ArcadeRecordHelper.new(_stages_data)
 	$MenuSceneControl._ContentContainerControl.add_child(_play_menu_content)
 	_stage_prelude.anchor_left = 0
 	_stage_prelude.anchor_right = 1
@@ -91,91 +93,13 @@ func _play_stage(data: ArcadeStageData) -> void:
 		visual_parameters,
 		data.get_uuid(),
 		funcref(self, "_back_to_play_menu"),
-		funcref(self, "_save_new_record")
+		funcref(_arcade_record_helper, "_save_new_record")
 	)
 	game_view.set_controller(game_controller)
 	game_controller.set_view(game_view, scale)
 	_main_scene_instance.add_game_controller(game_controller)
 	_main_scene_instance.stop_menu_music()
 	game_controller.start_new_game()
-
-func _save_new_record(uuid: String, stage_result: StageResult) -> void:
-	var difficulty: String = PersistentPlaySettings.get_difficulty()
-	var current_record: ArcadeRecord = _stages_data[uuid]
-	if current_record == null:
-		var new_record: ArcadeRecord
-		if difficulty == PersistentPlaySettings.NOOB:
-			new_record = ArcadeRecord.new(stage_result, stage_result, null, null, null, null)
-		elif difficulty == PersistentPlaySettings.REGULAR:
-			new_record = ArcadeRecord.new(null, null, stage_result, stage_result, null, null)
-		else:
-			new_record = ArcadeRecord.new(null, null, null, null, stage_result, stage_result)
-		PersistentArcadeStagesData.set_new_record(
-			uuid,
-			new_record
-		)
-		return
-	var current_length_record: StageResult = current_record.get_length_record(difficulty)
-	var current_score_record: StageResult = current_record.get_score_record(difficulty)
-	
-	var new_length_record = null
-	if current_length_record == null:
-		new_length_record = stage_result
-	elif stage_result.get_length() > current_length_record.get_length():
-		new_length_record = stage_result
-	elif stage_result.get_length() == current_length_record.get_length():
-		if stage_result.get_score() > current_length_record.get_score():
-			new_length_record = stage_result
-		elif stage_result.get_score() == current_length_record.get_score():
-			if stage_result.get_time() < current_length_record.get_time():
-				new_length_record = stage_result
-	
-	var new_score_record = null
-	if current_score_record == null:
-		new_score_record = stage_result
-	elif stage_result.get_score() > current_score_record.get_score():
-		new_score_record = stage_result
-	elif stage_result.get_score() == current_score_record.get_score():
-		if stage_result.get_length() > current_score_record.get_length():
-			new_score_record = stage_result
-		elif stage_result.get_length() == current_score_record.get_length():
-			if stage_result.get_time() < current_score_record.get_time():
-				new_score_record = stage_result
-	
-	if new_length_record != null || new_score_record != null:
-		if new_length_record != null:
-			current_length_record = new_length_record
-		if new_score_record != null:
-			current_score_record = new_score_record
-		var new_record: ArcadeRecord
-		if difficulty == PersistentPlaySettings.NOOB:
-			new_record = ArcadeRecord.new(
-				current_score_record,
-				current_length_record,
-				current_record.get_score_record(PersistentPlaySettings.REGULAR),
-				current_record.get_length_record(PersistentPlaySettings.REGULAR),
-				current_record.get_score_record(PersistentPlaySettings.PRO),
-				current_record.get_length_record(PersistentPlaySettings.PRO)
-			)
-		elif difficulty == PersistentPlaySettings.REGULAR:
-			new_record = ArcadeRecord.new(
-				current_record.get_score_record(PersistentPlaySettings.NOOB),
-				current_record.get_length_record(PersistentPlaySettings.NOOB),
-				current_score_record,
-				current_length_record,
-				current_record.get_score_record(PersistentPlaySettings.PRO),
-				current_record.get_length_record(PersistentPlaySettings.PRO)
-			)
-		else:
-			new_record = ArcadeRecord.new(
-				current_record.get_score_record(PersistentPlaySettings.NOOB),
-				current_record.get_length_record(PersistentPlaySettings.NOOB),
-				current_record.get_score_record(PersistentPlaySettings.REGULAR),
-				current_record.get_length_record(PersistentPlaySettings.REGULAR),
-				current_score_record,
-				current_length_record
-			)
-		PersistentArcadeStagesData.set_new_record(uuid, new_record)
 
 func _get_difficulty_settings_values() -> DifficultySettings:
 	var difficulty: String = PersistentPlaySettings.get_difficulty()
